6593b172bf68a1aa1ad97f2dbd1a993adcebfee4
RealCryZed <bujhmgtnhjd@bk.ru>
2020-03-19 16:59:35 +0600

    Added home page and first set ups for application

---
+++ src/main/java/site/Configs/WebConfig.java
@@ -0,0 +1,19 @@
+package site.Configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * Creates abbreviations of the url pages for further using in Controllers.
+     * @param registry adds new ViewController entities
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
